fc5540bb2bc7fed055fbd2ff6cbd24233cec2a38
Manoj Bharadwaja <bharadwaja1.618@gmail.com>
2025-10-15 11:17:44 +0530

    feat: add functions for various operations
    created functions for mathematical operations and logged them to check the functionality

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(4, 5));
+console.log(subtract(10, 4));
+console.log(multiply(18, 8));
+console.log(divide(64, 8));
